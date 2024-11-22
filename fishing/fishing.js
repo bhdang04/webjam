57227c163423ab2bcf722c8c94d75841db13b3a0
Peter Le <dedsnailyghost13@gmail.com>
2024-11-22 15:20:29 -0800

    Implemented pond fishing mechanic

--- fishing/fishing.js
+++ fishing/fishing.js
@@ -1,31 +1,46 @@
 const petrs = [
-    "donald_duck_petr.png",
-    "joker_petr.png",
-    "kermit_petr.png",
-    "normal_petr.png",
-    "shrek_petr.png"
+    ["donald_duck_petr.png", "Donald Duck Petr"],
+    ["joker_petr.png", "Joker Petr"],
+    ["kermit_petr.png", "Kemrit Petr"],
+    ["normal_petr.png", "Normal Petr"],
+    ["shrek_petr.png", "Shrek Petr"]
 ];
 
 document.addEventListener("DOMContentLoaded", () => {
     // Displays a random petr sticker
     document.getElementById('fish-button').addEventListener('click', () => {
-        
+        const petr_img = document.getElementById('petr-img');
+        const bobber = document.getElementById('bobber');
+        const prize_backdrop = document.getElementById('prize-backdrop');
+        const prize_message = document.getElementById('prize-message');
+
         // // Selects a random petr
         const randomIndex = Math.floor(Math.random() * petrs.length);
         const randomPetr = petrs[randomIndex];
 
         // // Projects chosen petr on screen
-        const petr_img = document.getElementById('petr-img');
-        petr_img.src = `images/petrs/${randomPetr}`;
+        petr_img.src = `images/petrs/${randomPetr[0]}`;
         petr_img.style.display = 'block';
-
-        // Slides bobber down from top
-        const imageContainer = document.querySelector('.image-container');
-        imageContainer.style.top = '-35%'; 
 
         // Restarts animation for next petr
         petr_img.classList.remove('appear');
-        void petr_img.offsetWidth; // Force reflow to restart CSS transition
+        void petr_img.offsetWidth; 
         petr_img.classList.add('appear');
+
+        // Restarts animation for bobber
+        bobber.classList.remove('fish');
+        void bobber.offsetWidth; 
+        bobber.classList.add('fish');
+
+        // Restarts animation for prize backdrop
+        prize_backdrop.classList.remove('appear');
+        void prize_backdrop.offsetWidth; 
+        prize_backdrop.classList.add('appear');
+
+        // Restarts animation for prize message
+        prize_message.innerText = `You got... ${randomPetr[1]}!`
+        prize_message.classList.remove('appear');
+        void prize_message.offsetWidth; 
+        prize_message.classList.add('appear');
     });
 }); 
